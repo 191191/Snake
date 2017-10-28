8842d8827385a4b9c55b9bfbea178fc9ccae0e6e
191191 <bkjyjxrf2@yandex.ru>
2017-10-28 14:08:52 +0300

    релиз2

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -16,6 +16,10 @@ namespace Snake
              Point p2 = new Point(4, 5, '#');
              p2.Draw();
 
+            Point p3 = new Point(5, 6, '@');
+            Point p4 = new Point(6, 7, '$');
+
+
             List<int> numList = new List<int>();
             numList.Add(0);
             numList.Add(1);
@@ -30,11 +34,28 @@ namespace Snake
                 Console.WriteLine(i);
             }
 
-            numList.RemoveAt(0);
+            /*List<char> charList = new List<char>();
+            charList.Add(0);
+            charList.Add(1);
+            charList.Add(2);
+
+            char a = charList['@'];
+            char b = charList['#'];
+            char c = charList['*'];
+            foreach (int i in charList)
+            {
+                Console.WriteLine(i);*/
 
             List<Point> pList = new List<Point>();
             pList.Add(p1);
             pList.Add(p2);
+            pList.Add(p3);
+            pList.Add(p4);
+
+            foreach (Point i in pList)
+            {
+                Console.WriteLine(i);
+            }
 
             Console.ReadLine();
 
